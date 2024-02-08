843b55f4c41ef3258e9a256805312a2a4f727d3d
J-onatha-n <91679080+J-onatha-n@users.noreply.github.com>
2024-02-08 16:44:14 -0500

    added a state machine

--- project1/Assets/Assets/Scripts/gameManager.cs
+++ project1/Assets/Assets/Scripts/gameManager.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class gameManager : MonoBehaviour
@@ -5,7 +8,8 @@ public class gameManager : MonoBehaviour
     [Header("Global vars")]
     public GameObject myPlayer;
     public float timer;
-    public int score; 
+    public float timeLimit;
+    public int score;
 
     [Header("NPC vars")]
     public GameObject collectible1;
@@ -13,29 +17,94 @@ public class gameManager : MonoBehaviour
     public float spawnTimer;
     public Vector2 spawnXBounds;
     public Vector2 spawnYBounds;
+
+    [Header("UI/UX Vars")]
+    public TextMeshProUGUI TitleText;
+
+    //to design a state machine, first we need to define a subclass of enum - GameState
+    public enum GameState
+    {
+        GAMESTART, PLAYING, GAMEOVER //we can pass it as many states as we want here
+    };
+    //declare an actual instance of GameState for the gameManager to use
+    public GameState myGameState;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        myGameState = GameState.GAMESTART;
+        myPlayer.SetActive(false);
+        TitleText.text = "Press [SPACE] to start";
     }
 
     // Update is called once per frame
     void Update()
     {
-        //timer is global, spawnTimer tracks collectibles
-        timer += Time.deltaTime;
-        spawnTimer += Time.deltaTime;
-
-        //this is the world position where our collectible spawns
-        float x = Random.Range(spawnXBounds.x, spawnXBounds.y);
-        float y = Random.Range(spawnYBounds.x, spawnYBounds.y);
-        Vector3 targetPos = new Vector3(x, y, 0);
-
-        //instantiate and reset timer when condition is met
-        if (spawnTimer > spawnInterval)
+        //switch statements work kind of like a lightswitch with 2 or more positions
+        switch (myGameState)
         {
-            Instantiate(collectible1, targetPos, Quaternion.identity);
-            spawnTimer = 0;
+            //each case can be seen as a single unique position of the lightswitch
+            case GameState.GAMESTART:
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    EnterPlaying();
+                }
+                break;
+
+            //this code only executes when our myGameState enum is currently in the matching state
+            //to the case in question
+            case GameState.PLAYING:
+                //timer is global, spawnTimer tracks collectibles
+                #region PLAYING_code
+                timer += Time.deltaTime;
+                spawnTimer += Time.deltaTime;
+
+                //check against timeLimit, end the game if we're at time
+                if (timer > timeLimit)
+                {
+                    EnterFinale();
+                }
+
+                //this is the world position where our collectible spawns
+                float x = Random.Range(spawnXBounds.x, spawnXBounds.y);
+                float y = Random.Range(spawnYBounds.x, spawnYBounds.y);
+                Vector3 targetPos = new Vector3(x, y, 0);
+
+                //instantiate and reset timer when condition is met
+                if (spawnTimer > spawnInterval)
+                {
+                    Instantiate(collectible1, targetPos, Quaternion.identity);
+                    spawnTimer = 0;
+                }
+                #endregion
+                break;
+
+            case GameState.GAMEOVER:
+                //code for the ending goes here
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    EnterPlaying();
+                }
+
+                break;
+
         }
+    }
+
+    //state change for playing mode, turn on players, disable any start menu logic
+    void EnterPlaying()
+    {
+        timer = 0f;
+        myGameState = GameState.PLAYING;
+        myPlayer.SetActive(true);
+        TitleText.enabled = false;
+    }
+
+    void EnterFinale()
+    {
+        myGameState = GameState.GAMEOVER;
+        myPlayer.SetActive(false);
+        TitleText.enabled = true;
+        TitleText.text = "CONGRATS, You Survived. Press [SPACE] to restart";
     }
 }
